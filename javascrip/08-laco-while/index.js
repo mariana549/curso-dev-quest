0b7ecb086a762267811c7c18b8dfd95c0e61260c
mariana549 <marievaristo889@gmail.com>
2023-12-04 17:31:17 -0300

    :sparkles:  atualizando
    novas aulas

---
+++ javascrip/08-laco-while/index.js
@@ -0,0 +1,20 @@
+/*
+while(condição){
+   <bloco de execução>
+}
+*/
+
+let a = 10 
+let b = 15
+
+while(a < b){
+   console.log('incrmentando a variavel a ' + a)
+   a++;
+}
+
+var i = 1
+
+do{
+   console.log("entrou " + i)
+   i++;
+}while(i <= 5)
---
+++ javascrip/09-condicionais/index.js
@@ -0,0 +1,14 @@
+let nomeFilme = 'batman'
+
+if(nomeFilme === 'vingadores'){
+   console.log('é o filme dos vingadores')
+}else if(nomeFilme === 'batman'){
+   console.log('é o filme do batman')
+}else {
+   console.log('NÃO E')
+}
+
+// operador ternario
+// false ? console.log('condição verdadeira') : console.log('condição falsa')
+
+nomeFilme === "vingadores" ? console.log('e o filme dos vingadores') : console.log('não o filme')
