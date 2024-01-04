8cf72be7cc1f7253311666030d0d10fbc7a5c448
mariana549 <marievaristo889@gmail.com>
2024-01-04 08:56:20 -0300

    aula 13 concluida

---
+++ javascript-intermediario/13 diferença entre objeto e arry/index.js
@@ -0,0 +1,14 @@
+let frutas = ['banana', 'uva', 'melão'];
+
+let videoGame ={
+   nome: 'xbox',
+   valor: 3000
+}
+
+let videoGame2 ={
+   nome: 'playstation',
+   valor: 5000
+}
+
+let videoGames = [videoGame, videoGame2]
+console.log(videoGames[1].nome)
