cf701e4fa7b95e8b7107d3009cc6ef2c9855d425
mariana549 <marievaristo889@gmail.com>
2024-01-05 12:58:44 -0300

    aula 20 concluida

---
+++ javascript-intermediario/20-resovendo-erros/index.js
@@ -0,0 +1,15 @@
+// var valorBoleto = 100;
+ 
+// console.log(valorBoleto);
+// console.log(valorBolet);
+
+
+let inputTelefone = document.getElementById("telefone");
+
+console.log(inputTelefone);
+
+inputTelefone.disabled = true;
+
+function habilitarTelefone() {
+  inputTelefone.disabled = false;
+}
