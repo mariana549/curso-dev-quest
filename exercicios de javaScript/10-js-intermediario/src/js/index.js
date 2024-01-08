d7635963c825e6a6a7a683010e0e85577b84908a
mariana549 <marievaristo889@gmail.com>
2024-01-08 15:23:16 -0300

    🚧 atualizando

---
+++ exercicios de javaScript/10-js-intermediario/src/js/index.js
@@ -0,0 +1,8 @@
+let buttonVoltarPagina = document.querySelector('.esquerdaSeta');
+let buttonProximaPagina = document.querySelector('.direitaSeta');
+let infor1 = document.querySelector('.info1')
+let titulo = document.querySelector('.tituloPrincipal')
+let paragrafro = document.querySelector('.descricao')
+let linkSaibaMais = document.querySelector('.link')
+
+
