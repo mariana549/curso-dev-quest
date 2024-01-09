196aed0e040aeebcb96739635b028cdef89a5ca4
mariana549 <marievaristo889@gmail.com>
2024-01-09 08:58:47 -0300

    💄 atualizações

--- exercicios de javaScript/10-js-intermediario/src/js/index.js
+++ exercicios de javaScript/10-js-intermediario/src/js/index.js
@@ -1,8 +1,33 @@
-let buttonVoltarPagina = document.querySelector('.esquerdaSeta');
-let buttonProximaPagina = document.querySelector('.direitaSeta');
-let infor1 = document.querySelector('.info1')
 let titulo = document.querySelector('.tituloPrincipal')
 let paragrafro = document.querySelector('.descricao')
 let linkSaibaMais = document.querySelector('.link')
 
+function conteudo() {
+   let infor1 = document.querySelector('.info1')
+   let infor2 = document.querySelector('.info2')
+   let infor3 = document.querySelector('.info3')
+   let infor4 = document.querySelector('.info4')
+   let block = document.querySelector('.block')
+   let none = document.querySelector('.none')
 
+   let informacoes = [infor1, infor2, infor3, infor4];
+
+   let mostrar = informacoes.forEach(element => {
+      if (element === 'block'){
+            return(element)
+      }
+   });
+   console.log(mostrar)
+}
+conteudo()
+
+function mudarDePaisagem() {
+   let buttonVoltarPagina = document.querySelector('.esquerdaSeta');
+   let buttonProximaPagina = document.querySelector('.direitaSeta');
+   let ProximaPagina = buttonProximaPagina
+   let VoltarPagina = buttonVoltarPagina
+
+
+
+}
+mudarDePaisagem()
