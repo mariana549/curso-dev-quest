2bd81e17415d5e9380ba6818e47426826aa0bc1d
mariana549 <marievaristo889@gmail.com>
2024-01-09 09:14:36 -0300

    💄 mudanças no código

--- exercicios de javaScript/10-js-intermediario/src/js/index.js
+++ exercicios de javaScript/10-js-intermediario/src/js/index.js
@@ -1,33 +1,33 @@
-let titulo = document.querySelector('.tituloPrincipal')
-let paragrafro = document.querySelector('.descricao')
-let linkSaibaMais = document.querySelector('.link')
+// let titulo = document.querySelector('.tituloPrincipal')
+// let paragrafro = document.querySelector('.descricao')
+// let linkSaibaMais = document.querySelector('.link')
 
-function conteudo() {
-   let infor1 = document.querySelector('.info1')
-   let infor2 = document.querySelector('.info2')
-   let infor3 = document.querySelector('.info3')
-   let infor4 = document.querySelector('.info4')
-   let block = document.querySelector('.block')
-   let none = document.querySelector('.none')
+// function conteudo() {
+//    let infor1 = document.querySelector('.info1')
+//    let infor2 = document.querySelector('.info2')
+//    let infor3 = document.querySelector('.info3')
+//    let infor4 = document.querySelector('.info4')
+//    let block = document.querySelector('.block')
+//    let none = document.querySelector('.none')
 
-   let informacoes = [infor1, infor2, infor3, infor4];
+//    let informacoes = [infor1, infor2, infor3, infor4];
 
-   let mostrar = informacoes.forEach(element => {
-      if (element === 'block'){
-            return(element)
-      }
-   });
-   console.log(mostrar)
-}
-conteudo()
+//    let mostrar = informacoes.forEach(element => {
+//       if (element === 'block'){
+//             return(element)
+//       }
+//    });
+//    console.log(mostrar)
+// }
+// conteudo()
 
-function mudarDePaisagem() {
-   let buttonVoltarPagina = document.querySelector('.esquerdaSeta');
-   let buttonProximaPagina = document.querySelector('.direitaSeta');
-   let ProximaPagina = buttonProximaPagina
-   let VoltarPagina = buttonVoltarPagina
+// function mudarDePaisagem() {
+//    let buttonVoltarPagina = document.querySelector('.esquerdaSeta');
+//    let buttonProximaPagina = document.querySelector('.direitaSeta');
+//    let ProximaPagina = buttonProximaPagina
+//    let VoltarPagina = buttonVoltarPagina
 
 
 
-}
-mudarDePaisagem()
+// }
+// mudarDePaisagem()
