838370d7cf4b62b81a2f587683ba82cf54116da1
mariana549 <marievaristo889@gmail.com>
2024-01-30 14:28:59 -0300

    atualização

---
+++ javascript-avancado/06-operador-spread/index.js
@@ -0,0 +1,19 @@
+const pessoas =  ['mariana', 'ricardo', 'marcos']
+// console.log(...pessoas)
+
+// let pessoas1 = ['mariana', 'ricardo', 'marcos']
+// let pessoas2 = ['mariana', 'ricardo', 'marcos']
+
+// // pessoas1 = pessoas1.concat(pessoas2)
+
+// pessoas1 = [...pessoas1, ...pessoas2]
+// console.log(pessoas1)
+
+// CLONAR OBJETO
+
+// const pessoas1 = {nome: 'mariana', idade: 20}
+
+// const objetoClonado = {...pessoas1}
+// console.log(objetoClonado)
+// console.log(pessoas1)
+
