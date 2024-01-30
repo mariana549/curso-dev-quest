64495234d1392e99905c72a97c885d70783200cb
mariana549 <marievaristo889@gmail.com>
2024-01-30 14:28:46 -0300

    atualização

---
+++ javascript-avancado/04-templete-string/index.js
@@ -0,0 +1,12 @@
+// const imprimirInformacoesPessoa = (primeiroNome, ultimoNome, idade) => {
+//    return "Primeiro Nome: " + primeiroNome + ", ultimo Nome: " + ultimoNome + ", Idade: " + idade
+// }
+// console.log(imprimirInformacoesPessoa("roberto", "dias", 33))
+
+
+
+const imprimirInformacoesPessoa = (primeiroNome, ultimoNome, idade) => {
+   return `Primeiro Nome: ${primeiroNome}, ultimo Nome: ${ultimoNome}, Idade: ${idade}`
+}
+// console.log(imprimirInformacoesPessoa("roberto", "dias", 33))
+console.log(`imprimirInformacoesPessoa("roberto", "dias", 33), ele é um devquester`)
