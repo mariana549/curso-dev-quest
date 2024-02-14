ac8e2420fdbe8eb0c6212b4a4e2427e52b171976
mariana549 <marievaristo889@gmail.com>
2024-02-14 14:20:52 -0300

    atualização

--- modulo-teste-basico/calcular-valores-pedidos/calcular-valores-pedidos.js
+++ modulo-teste-basico/calcular-valores-pedidos/calcular-valores-pedidos.js
@@ -1,9 +1,24 @@
 const meuPedido = {
    itens : [
       {nome: 'poção de vida', valor: 100},
-      {nome: 'Espada de prata', valor: 1000}
+      {nome: 'Espada de prata', valor: 1000},
+      {nome: 'entrega', valor: 40, entrega: true}
    ]
 }
-const calcularValorPedido = pedido => 
-pedido.itens.reduce((totalpedidos, pedidoAtual) => totalpedidos + pedidoAtual.valor, 0)
+const calcularValorPedido = pedido => {
+   const valorProdutos = pedido.itens
+   .filter(item => !item.entrega)
+   .reduce((totalpedidos, pedidoAtual) => totalpedidos + pedidoAtual.valor, 0)
+
+   const entrega = pedidos.itens.filter(item => item.entrega);
+
+   if (valorProdutos > 500){
+      return valorProdutos
+   } else {
+      return valorProdutos + entrega.valor
+   }
+}
+
+// pedido.itens.reduce((totalpedidos, pedidoAtual) => totalpedidos + pedidoAtual.valor, 0)
+
 console.log(calcularValorPedido(meuPedido)) //1100
