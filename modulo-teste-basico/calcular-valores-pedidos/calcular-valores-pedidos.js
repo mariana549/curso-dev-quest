9ac5f889a9b8b6707104875aeec79b32a1cbc432
mariana549 <marievaristo889@gmail.com>
2024-02-15 10:00:19 -0300

    refactor: :recycle: refatorando
    alterando trecho de if e else para uma unica linha
    
    mariana antonia

--- modulo-teste-basico/calcular-valores-pedidos/calcular-valores-pedidos.js
+++ modulo-teste-basico/calcular-valores-pedidos/calcular-valores-pedidos.js
@@ -5,11 +5,7 @@ const calcularValorPedido = pedido => {
 
    const entrega = pedido.itens.filter(item => item.entrega);
 
-   if (valorProdutos > 500){
-      return valorProdutos
-   } else {
-      return valorProdutos + entrega[0].valor
-   }
+   return (valorProdutos > 500) ? valorProdutos : valorProdutos + entrega[0].valor;
 }
 
 module.exports = calcularValorPedido
