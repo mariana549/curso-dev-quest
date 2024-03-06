593fdeb9c82907124edcd5e10bb1a4866caef5e3
mariana549 <marievaristo889@gmail.com>
2024-03-06 09:54:48 -0300

    atualização

--- react/05-creaat-react-app/primeiro-app/src/App.js
+++ react/05-creaat-react-app/primeiro-app/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>olá mundo!</h1>
+    </>
   );
 }
 
 export default App;
+
+
+{/* <header className="App-header">
+<img src={logo} className="App-logo" alt="logo" />
+<p>
+  ola, eu sou a mariana como vai?
+</p>
+<a
+  className="App-link"
+  href="https://reactjs.org"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  Learn React
+</a>
+</header> */}
