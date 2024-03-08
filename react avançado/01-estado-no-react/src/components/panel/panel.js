eb1778422bed688779dfa7eeb771c90f2abaeff3
mariana549 <marievaristo889@gmail.com>
2024-03-08 14:19:04 -0300

    update

---
+++ react avançado/01-estado-no-react/src/components/panel/panel.js
@@ -0,0 +1,14 @@
+import  { Component } from "react"
+import "../panel/panel.css"
+
+class Panel extends Component {
+   render() {
+      return (
+         <section className="panel">
+            <h2>Titulo do painel</h2>
+         </section>
+      )
+   }
+}
+
+export default Panel
