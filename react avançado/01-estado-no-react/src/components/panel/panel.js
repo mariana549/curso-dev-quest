21a28a3d007dcd20ea9e750f8c6691e288249949
mariana549 <marievaristo889@gmail.com>
2024-03-08 14:31:50 -0300

    atualização

--- react avançado/01-estado-no-react/src/components/panel/panel.js
+++ react avançado/01-estado-no-react/src/components/panel/panel.js
@@ -1,11 +1,18 @@
 import  { Component } from "react"
 import "../panel/panel.css"
 
-class Panel extends Component {
+class Panel extends Component{
+   constructor(){
+      super()
+      this.state = {
+         title: "Titulo do painel",
+      }
+   }
+
    render() {
       return (
-         <section className="panel">
-            <h2>Titulo do painel</h2>
+         <section className="panel" onClick={() => this.setState({title: "Titulo novo"})}>
+            <h2>{this.state.title}</h2>
          </section>
       )
    }
