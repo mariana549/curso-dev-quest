6cb43ebeb82d5c4e18e0e4a6fd483d4b096f6177
mariana549 <marievaristo889@gmail.com>
2024-03-13 11:40:42 -0300

    chamando api deck cards

--- react avançado/01-estado-no-react/src/components/deck-of-cards/deck-of-card.js
+++ react avançado/01-estado-no-react/src/components/deck-of-cards/deck-of-card.js
@@ -1,5 +1,16 @@
 import { Component } from 'react'
 
+async function createDeck(){
+   const response = await fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+   const deck = await response.json()
+   return deck.deck_id
+}
+
+async function getCards(deckId){
+   const response = await fetch(`https://deckofcardsapi.com/api/${deckId}/draw/?count=52`)
+   return await response.json()
+}
+
 class DeckOfCards extends Component {
    constructor () {
       super()
