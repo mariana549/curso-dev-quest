98fb4de3ef5ac0dae7ad49911b74f9b6fed61816
mariana549 <marievaristo889@gmail.com>
2024-03-14 10:17:32 -0300

    refatorando

--- react avançado/01-estado-no-react/src/components/deck-of-cards/deck-of-card.js
+++ react avançado/01-estado-no-react/src/components/deck-of-cards/deck-of-card.js
@@ -1,12 +1,12 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
-async function createDeck(){
+async function createDeck() {
    const response = await fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
    const deck = await response.json()
    return deck.deck_id
 }
 
-async function getCards(deckId){
+async function getCards(deckId) {
    const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=52`)
    return await response.json()
 }
@@ -15,6 +15,18 @@ const DeckOfCards = () => {
    const [deck, setDeck] = useState({
       cards: []
    })
+
+   useEffect(() => {
+      const fetchData = async () => {
+         const deckId = await createDeck()
+         const data = await getCards(deckId)
+   
+         setDeck({
+            cards: data.cards
+         })
+      }
+      fetchData()
+   }, [])
 
    return (
       <section>
@@ -31,26 +43,6 @@ const DeckOfCards = () => {
          </ul>
       </section>
    )
-
-   // constructor () {
-   //    super()
-   //    this.state = {
-   //       cards: []
-   //    }
-   // }
-
-   // async componentDidMount(){
-   //    const deckId = await createDeck()
-   //    const data = await getCards(deckId)
-
-   //    this.setState({
-   //        cards: data.cards
-   //       })
-   // }
-
-   // render() {
-   //    
-   // }
 }
 
 export default DeckOfCards 
