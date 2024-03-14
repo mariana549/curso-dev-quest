b21c0df73155a574d9a62ab178671db1de60aede
mariana549 <marievaristo889@gmail.com>
2024-03-14 11:40:27 -0300

    importando o form e atualizando informações no deck-of-card.js

--- react avançado/01-estado-no-react/src/components/deck-of-cards/deck-of-card.js
+++ react avançado/01-estado-no-react/src/components/deck-of-cards/deck-of-card.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import Form from '../form/form'
 
 async function createDeck() {
    const response = await fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
@@ -7,9 +8,11 @@ async function createDeck() {
 }
 
 async function getCards(deckId) {
-   const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=52`)
+   const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
    return await response.json()
 }
+
+
 
 const CardList = (props) => {
    return (
@@ -46,6 +49,7 @@ const DeckOfCards = () => {
 
    return (
       <section>
+         <Form />
          {deck.cards.length > 0 ? <CardList cards={deck.cards}/> : 'nenhuma carta encotrada'}
       </section>
    )
