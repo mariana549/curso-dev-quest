af083b7e96dea0e0db14780f9a5e1e2aa609f752
mariana549 <marievaristo889@gmail.com>
2024-03-19 10:00:29 -0300

    criando o routes

--- react avançado/projeto-blog/src/pages/post.js
+++ react avançado/projeto-blog/src/pages/post.js
@@ -1,7 +1,9 @@
 import { PostsList } from "../components/posts"
 
 const Post = () => {
-
+   return (
+      <PostsList />
+   )
 }
 
 export { Post } 
---
+++ react avançado/projeto-blog/src/pages/routes.js
@@ -0,0 +1,12 @@
+import { BrowserRouter, Route , Routes  } from "react-router-dom"
+import { Post } from "./post"
+
+const AppRoutes = () => {
+   <BrowserRouter>
+      <Routes>
+         <Route exact path="/" element={<Post />}/>
+      </Routes>
+   </BrowserRouter>
+}
+
+export { AppRoutes }
