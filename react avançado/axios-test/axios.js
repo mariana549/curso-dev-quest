3191a0a4a250cb770e6167371df5f4896c4b4e9c
mariana549 <marievaristo889@gmail.com>
2024-03-19 19:34:28 -0300

    refatorando usando o axios

--- react avançado/axios-test/axios.js
+++ react avançado/axios-test/axios.js
@@ -1,13 +1,18 @@
+import axios from "axios"
+
 export async function fetchUserDetails(element, username) {
    try {
-      const response = await fetch(`https://api.github.com/users/${username}`)
+      // const response = await fetch(`https://api.github.com/users/${username}`)
 
-      if (!response.ok) {
-         throw new Error(`Github API return status code ${response.status}`)
-      }
+      const response = await axios.get(`https://api.github.com/users/${username}`)
 
-      const user = await response.json()
-      console.log(user)
+      // if (!response.ok) {
+      //    throw new Error(`Github API return status code ${response.status}`)
+      // }
+      // const user = await response.json()
+      
+      const user = response.data;
+
       element.innerHTML = `
       <img src="${user.avatar_url}" alt="${user.name}" />
       <h1>${user.name ? user.name : "no name"}</h1>
