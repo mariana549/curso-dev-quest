7e2702e582516ce07a8eff3636165261a9fbff54
mariana549 <marievaristo889@gmail.com>
2024-03-19 11:11:31 -0300

    atualizando os estilos com styled

--- react avançado/projeto-blog/src/App.js
+++ react avançado/projeto-blog/src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import { AppRoutes } from './pages/routes';
+import { createGlobalStyle } from 'styled-components';
 
 function App() {
   return (
    <>
+      <GlobalStyle />
       <AppRoutes />
    </>
   );
 }
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+  }
+`
 
 export default App;
