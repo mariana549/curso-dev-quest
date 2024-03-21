700a02dbc46cff6869dac57cdc1263ae519c64a1
mariana549 <marievaristo889@gmail.com>
2024-03-21 14:47:58 -0300

    criado form

---
+++ habilidades-avancadas/react-custom-hooks/src/components/form/form.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export default function Form(){
+   const [firstName, setFirstName] = useState("Mari");
+   const [lastName, setLastName] = useState("Antonia");
+
+   function handleFirstNameChange(event) {
+      setFirstName(event.target.value);
+   }
+
+   function handleLastNameChange(event) {
+      setLastName(event.target.value);
+   }
+
+   return(
+      <>
+      <label>
+         First name:
+
+         <input value={firstName} onChange={handleFirstNameChange}></input>
+      </label>
+
+      <label>
+         Last name:
+
+         <input value={lastName} onChange={handleLastNameChange}></input>
+      </label>
+
+      <p>Bom dia, {firstName}{lastName}!</p>
+      </>
+   )
+}
