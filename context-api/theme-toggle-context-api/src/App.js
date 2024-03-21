e1f8055ad3e27fdb227e622c79f4416c72661a25
mariana549 <marievaristo889@gmail.com>
2024-03-21 17:33:15 -0300

    adionado o card dentro app

--- context-api/theme-toggle-context-api/src/App.js
+++ context-api/theme-toggle-context-api/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { ThemeProvider } from './contexts/theme-context';
 import { ThemeTogglerButton } from './components/theme-toggle-buttom/theme-toggle-buttom';
+import { Card } from './components/card/card';
 
 function App() {
   return (
     <ThemeProvider>
       <ThemeTogglerButton />
+      <Card/>
     </ThemeProvider>
     );
 }
