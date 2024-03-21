8c9e45af081077f8d7d6e202f215d60b1d4f4e63
mariana549 <marievaristo889@gmail.com>
2024-03-21 17:31:38 -0300

    adicionando estilos

--- context-api/theme-toggle-context-api/src/components/card/card.js
+++ context-api/theme-toggle-context-api/src/components/card/card.js
@@ -1,6 +1,11 @@
+import React, {useContext} from "react"
+import { ThemeContext } from "../../contexts/theme-context"
+
 export const Card = () => {
+   const { theme } = useContext(ThemeContext)
+
    return (
-      <div>
+      <div style={{ color: theme.color, background: theme.background }}>
          <h1>Titulo do card</h1>
          <p>Paragrafo do card, Paragrafo do card, Paragrafo do card.</p>
       </div>
