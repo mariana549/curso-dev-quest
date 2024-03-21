142e9356f7c2ea94815025906cdc417e76a8ef67
mariana549 <marievaristo889@gmail.com>
2024-03-21 17:00:30 -0300

    importando o button

--- context-api/theme-toggle-context-api/src/components/theme-toggle-buttom/theme-toggle-buttom.js
+++ context-api/theme-toggle-context-api/src/components/theme-toggle-buttom/theme-toggle-buttom.js
@@ -1,12 +1,13 @@
 import React, { useContext } from "react"
 import { ThemeContext } from "../../contexts/theme-context"
+import { Button } from "../button/button"
 
 export const ThemeTogglerButton = () => {
    const { themes } = useContext(ThemeContext)
    console.log('themesToggleButton', themes)
    return (
       <div>
-         
+         <Button />
       </div>
    )
 }
