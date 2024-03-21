5965803f4aef87d08e71545561146ab4058ab8aa
mariana549 <marievaristo889@gmail.com>
2024-03-21 17:29:05 -0300

    criado card

---
+++ context-api/theme-toggle-context-api/src/components/card/card.js
@@ -0,0 +1,8 @@
+export const Card = () => {
+   return (
+      <div>
+         <h1>Titulo do card</h1>
+         <p>Paragrafo do card, Paragrafo do card, Paragrafo do card.</p>
+      </div>
+   )
+} 
