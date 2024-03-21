bdf4a33b7032cc7b321f41a568cf757e49fb2326
mariana549 <marievaristo889@gmail.com>
2024-03-21 17:04:29 -0300

    atualizando button

--- context-api/theme-toggle-context-api/src/components/button/button.js
+++ context-api/theme-toggle-context-api/src/components/button/button.js
@@ -1,4 +1,9 @@
+import React, { useContext } from "react"
+import { ThemeContext } from "../../contexts/theme-context"
+
 export const Button = (props) => {
+   const {themes} = useContext(ThemeContext)
+   console.log('button themes', themes)
    return (
       <button {...props}/>
    )
