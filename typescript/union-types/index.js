02cb8cf86ba1d99b11e3db4846a1b42341a317ba
Mariana Antonia <marievaristo889@gmail.com>
2024-07-23 11:39:30 -0300

    atualizando

---
+++ typescript/union-types/index.js
@@ -0,0 +1,5 @@
+function printClientId(id) {
+    console.log("o id do cliente \u00E9: ".concat(id));
+}
+printClientId(100);
+printClientId("mariana");
---
+++ typescript/union-types/index.ts
@@ -0,0 +1,6 @@
+function printClientId(id: number | string): void {
+   console.log(`o id do cliente é: ${id}`);
+}
+
+printClientId(100)
+printClientId("mariana")
