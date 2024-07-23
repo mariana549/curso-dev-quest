0e6d856538a66f56041da4beda0dd103271cd84a
Mariana Antonia <marievaristo889@gmail.com>
2024-07-23 11:26:31 -0300

    atualizar

---
+++ typescript/any/index.js
@@ -0,0 +1,6 @@
+// let moviesCount: any = 10;
+// moviesCount = "10"
+function sum(a, b) {
+    return a + b;
+}
+console.log(10, 90);
---
+++ typescript/any/index.ts
@@ -0,0 +1,9 @@
+// let moviesCount: any = 10;
+
+// moviesCount = "10"
+
+function sum(a: any, b: any) {
+   return a + b;
+}
+
+console.log(10,90)
